1657fb6ba951253ae10906d4be7481e4aabacc43
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-11-11 01:38:30 -0800

    need to fix transition

--- js/main.js
+++ js/main.js
@@ -1,4 +1,60 @@
+var svg = d3.select("svg"),
+margin = {top: 40, right: 40, bottom: 40, left: 40},
+width = svg.attr("width") - margin.left - margin.right,
+height = svg.attr("height") - margin.top - margin.bottom;
+var toggles = d3.select(".container").append("div")
+    .attr("class","histogram-chart-toggle-wrapper");
+
+
+var percentScale = d3.scaleLinear()
+.rangeRound([0, width]);
+
+var parityScale = d3.scaleLinear()
+.rangeRound([0, width]);
+
+
+
+var g = svg.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 d3.csv('data/data.csv', type, function(error, data) {
+	toggles
+    .append("div")
+    .attr("class","histogram-chart-toggle-type")
+    .selectAll("p")
+    .data(["Gender","Race"])
+    .enter()
+    .append("p")
+    .attr("class","histogram-chart-toggle-item")
+    .text(function(d){
+      return d;
+    })
+     .attr("class",function(d,i){
+        if(i==0){
+          return "toggle-selected front-curve histogram-chart-toggle-item";
+        }
+        if(i==1){
+          return "back-curve histogram-chart-toggle-item";
+        }
+        return "histogram-chart-toggle-item";
+      })
+      .text(function(d){
+        return d;
+      })
+      .on("click",function(d){
+
+
+        var dataSelected = d;
+        d3.select(this.parentNode).selectAll("p").classed("toggle-selected",function(d){
+          if(d==dataSelected){
+            return true;
+          }
+          return false;
+        })
+        countMin = d;
+        console.log(d);
+        buildChart(d);
+      });
 
 
 	men = data.filter(function(d) { return d.gender == "male"});
@@ -19,10 +75,21 @@ d3.csv('data/data.csv', type, function(error, data) {
 						totalasian: d3.sum(v, function(d) { if (d.race == "Asian" || d.race == "Native_Hawaiian_or_Pacific_Islander") { return d.count; }}),
 						totalamerindian: d3.sum(v, function(d) { if (d.race == "American_Indian_Alaskan_Native") { return d.count; }}),
 						totalmixed: d3.sum(v, function(d) { if (d.race == "Two_or_more_races") { return d.count; }}),
-					 	total: d3.sum(v, function(d) { return d.count; })
+					 	total: d3.sum(v, function(d) { return d.count; }),
+					 	totalmenleaders: d3.sum(v, function(d) { if (d.gender == "male" && d.job_category == "Executive/Senior officials & Mgrs") {return d.count; }}),
+					 	totalwomenleaders: d3.sum(v, function(d) { if (d.gender == "female" && d.job_category == "Executive/Senior officials & Mgrs") {return d.count; }}),
+					 	totalwhiteleaders: d3.sum(v, function(d) { if (d.race == "White" && d.job_category == "Executive/Senior officials & Mgrs") {return d.count; }}),
+					 	totalblackleaders: d3.sum(v, function(d) { if (d.race == "Black_or_African_American" && d.job_category == "Executive/Senior officials & Mgrs") {return d.count; }}),
+					 	totallatinoleaders: d3.sum(v, function(d) { if (d.race == "Latino" && d.job_category == "Executive/Senior officials & Mgrs") {return d.count; }}),
+					 	totalasianleaders: d3.sum(v, function(d) { if ((d.race == "Asian" || d.race == "Native_Hawaiian_or_Pacific_Islander") && d.job_category == "Executive/Senior officials & Mgrs") {return d.count; }}),
+					 	totalamerindianleaders: d3.sum(v, function(d) { if (d.race == "American_Indian_Alaskan_Native" && d.job_category == "Executive/Senior officials & Mgrs") {return d.count; }}),
+					 	totalmixedleaders: d3.sum(v, function(d) { if (d.race == "American_Indian_Alaskan_Native" && d.job_category == "Executive/Senior officials & Mgrs") {return d.count; }}),
+					 	totalleaders: d3.sum(v, function(d) { if (d.job_category == "Executive/Senior officials & Mgrs") {return d.count; }})
 					}})
 					.entries(noTotalsData);
-	percentagesByCompany = totalsByCompany.map(function(d) { return {key: d.key, value: {percentmen: d.value.totalmen/d.value.total, 
+	console.log(totalsByCompany);
+	percentagesByCompany = totalsByCompany.map(function(d) { return {key: d.key, value: { total: d.value.total,
+																						percentmen: d.value.totalmen/d.value.total, 
 																						percentwomen: d.value.totalwomen/d.value.total, 
 																						percentwhite: d.value.totalwhite/d.value.total, 
 																						percentblack: d.value.totalblack/d.value.total, 
@@ -30,6 +97,14 @@ d3.csv('data/data.csv', type, function(error, data) {
 																						percentasian: d.value.totalasian/d.value.total, 
 																						percentamerindian: d.value.totalamerindian/d.value.total, 
 																						percentmixed: d.value.totalmixed/d.value.total,
+																						percentmenleaders: d.value.totalmenleaders/d.value.totalleaders, 
+																						percentwomenleaders: d.value.totalwomenleaders/d.value.totalleaders, 
+																						percentwhiteleaders: d.value.totalwhiteleaders/d.value.totalleaders, 
+																						percentblackleaders: d.value.totalblackleaders/d.value.totalleaders, 
+																						percentlatinoleaders: d.value.totallatinoleaders/d.value.totalleaders, 
+																						percentasianleaders: d.value.totalasianleaders/d.value.totalleaders, 
+																						percentamerindianleaders: d.value.totalamerindianleaders/d.value.totalleaders, 
+																						percentmixedleaders: d.value.totalmixedleaders/d.value.totalleaders
 																				
 																					}}});
 	for (var i = 0; i < percentagesByCompany.length; i++) {
@@ -37,12 +112,72 @@ d3.csv('data/data.csv', type, function(error, data) {
 		company.value.parity = -company.value.percentwhite + company.value.percentblack + company.value.percentlatino + company.value.percentasian + company.value.percentamerindian + company.value.percentmixed + 0.424 - 0.064-0.03-0.236-0.235-0.035;
 	}
 	
-	console.log(totalsByCompany);
+
 	console.log(percentagesByCompany);
 	dataByCompany = d3.nest()
 					.key(function(d) { return d.company})
 					.entries(data);
 	//console.log(dataByCompany);
+
+	var circleScale = d3.scaleLinear()
+   						.domain(d3.extent(percentagesByCompany, function(d) { return d.value.total;}))
+   						.range([3, 30])
+
+	//x.domain(d3.extent(percentagesByCompany, function(d) { return d.value.percentmen; }));
+	percentScale.domain([0,1]);
+	parityScale.domain(d3.extent(percentagesByCompany, function(d) { return d.value.parity; }));
+	
+	
+
+	buildChart("Gender")
+
+      
+    
+      function buildChart(toggle) {
+      
+      	var simulation = d3.forceSimulation(percentagesByCompany)
+      	.force("x", d3.forceX(function(d) { 
+	      	if (toggle == "Gender") {
+	      		return percentScale(d.value.percentmen); 
+	     	} else {
+	     		return parityScale(d.value.parity); 
+	     	}
+	     }).strength(1))
+      .force("y", d3.forceY(height / 2))
+      .force("collide", d3.forceCollide().radius(function(d) { return circleScale(d.value.total) + 1; }))
+      .stop();
+
+    
+    for (var i = 0; i < 120; ++i) simulation.tick();
+
+   	/*chartAxis = g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(percentScale));*/
+
+   	var cell = g.append("g")
+      .attr("class", "cells")
+      .selectAll("g").data(percentagesByCompany)
+      .enter().append("g");
+    /*.selectAll("g").data(d3.voronoi()
+        .extent([[-margin.left, -margin.top], [width + margin.right, height + margin.top]])
+        .x(function(d) { return d.x; })
+        .y(function(d) { return d.y; })
+      .polygons(percentagesByCompany)).enter().append("g");*/
+
+      cell.append("circle")
+       .transition()
+        .duration(500)
+      .attr("r", function(d) { 
+      	console.log(d); 
+      	return circleScale(d.value.total);
+      	//return 3;
+      })
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+
+
+      }
 }) 
 
 
